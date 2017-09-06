ff2a80155869f140daa396e6938c515872506c0c
Kerry Buckley <kerryjbuckley@gmail.com>
2017-09-06 22:13:45 +0100

    Extract method

--- lib/sequence_progression/algebraic.ex
+++ lib/sequence_progression/algebraic.ex
@@ -2,12 +2,15 @@ defmodule SequenceProgression.Algebraic do
   @behaviour SequenceProgression.Solver
 
   def next(sequence) do
-    case sequence
-         |> Enum.chunk_every(2, 1, :discard)
-         |> Enum.map(fn [a, b] -> b - a end)
-         |> Enum.uniq do
+    case sequence |> diffs |> Enum.uniq do
       [diff] -> List.last(sequence) + diff
       _      -> nil
     end
+  end
+
+  defp diffs(sequence) do
+    sequence
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> b - a end)
   end
 end
