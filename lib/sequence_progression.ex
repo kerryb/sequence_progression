f527c2e39f17a7b886c8830ed0ebe8fd336ab82e
Kerry Buckley <kerryjbuckley@gmail.com>
2017-09-06 20:05:18 +0100

    Add kata description and [failing] doctest

--- lib/sequence_progression.ex
+++ lib/sequence_progression.ex
@@ -1,18 +1,50 @@
 defmodule SequenceProgression do
   @moduledoc """
-  Documentation for SequenceProgression.
-  """
+  # Sequence Progression
 
-  @doc """
-  Hello world.
+  For this problem you will need to write an app that will take a progressive
+  sequence of 5 numbers, work out how the sequence progresses and output the
+  next 1 number in the sequence.
 
   ## Examples
 
-      iex> SequenceProgression.hello
-      :world
+  iex> SequenceProgression.next [1, 3, 5, 7, 9]
+  11
 
+  (Progression here is: +2 previous value)
+
+  The basic app should work out sequences that simply add/subtract a number to
+  their elements just like the above example
+
+  If you want to go further, try doing multiplication/division, eg:
+
+      Input_2:  2, 4, 8, 16, 32
+      Output_2: 64
+
+  (Progression here is: x2 previous value)
+
+      Input_3: 1024, -512, 256, -128, 64 O
+      utput_3: -32
+
+  (Progression here is: /-2 previous value)
+
+  ### Challenges:
+
+  More advanced sequences could include squares/roots of numbers and any other
+  thing you could come up with, such as quadratic functions.
+
+  You could also have a blank number in the sequence, eg:
+
+    Input_4:  1,  _, 9, 16, 25
+    Output_4: 36
+
+  (Progression here is: ^2 of its index)
   """
-  def hello do
-    :world
+
+  @solvers []
+
+  def next(sequence, solvers\\@solvers) do
+    solvers
+    |> Enum.find(fn solver -> solver.next sequence end)
   end
 end
--- test/sequence_progression_test.exs
+++ test/sequence_progression_test.exs
@@ -1,8 +1,4 @@
 defmodule SequenceProgressionTest do
   use ExUnit.Case
   doctest SequenceProgression
-
-  test "greets the world" do
-    assert SequenceProgression.hello() == :world
-  end
 end
